a2d6761daa7a761724b40a90530cf250e7763a65
Erick Garcia <garcia.erick.cs@gmail.com>
2018-07-12 13:20:48 -0600

    changes from laptop

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# cp vimrc ~/.vimrc
+cp vimrc ~/.vimrc
 
 # Installing vundle
-# mkdir -p ~/.vim/bundle/
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir -p ~/.vim/bundle/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Installing Vundle plugins
-# vim +PluginInstall +qall
+vim +PluginInstall +qall
 
 # Copying Colorscheme
 mkdir -p ~/.config/base16-shell
-cp ~/.vim/bundle/vim-hybrid-material/base16-material/base16-material.dark.sh ~/.config/base16-shell/
+cp base16-material.dark.sh ~/.config/base16-shell/
 mkdir -p ~/.vim/colors
-cp ~/.vim/bundle/vim-hybrid-material/base16-material/base16-material.vim ~/.vim/colors/
+cp base16-material.vim ~/.vim/colors/
+cp vimrc ~/.vimrc
 
 # # Python dependency
 # # Installing YouCompleteMe with C support
-# cd ~/.vim/bundle/YouCompleteMe
-# ./install.py --clang-completer
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
