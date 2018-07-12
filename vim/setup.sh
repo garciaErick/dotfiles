a818c8a89ac1085756c3766611ce24cc2514ace1
Erick Garcia <garcia.erick.cs@gmail.com>
2018-07-12 10:09:23 -0600

    Updated the script to correctly create the directories

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-cp vimrc ~/.vimrc
+# cp vimrc ~/.vimrc
 
-mkdir -p ~/.vim/bundle/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Installing vundle
+# mkdir -p ~/.vim/bundle/
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-vim +PluginInstall +qall
+# Installing Vundle plugins
+# vim +PluginInstall +qall
 
-cp ~/.vim/bundle/vim-hybrid-material/base16-material/base16-material.dark.sh ~/.config/base16-shell
+# Copying Colorscheme
+mkdir -p ~/.config/base16-shell
+cp ~/.vim/bundle/vim-hybrid-material/base16-material/base16-material.dark.sh ~/.config/base16-shell/
 mkdir -p ~/.vim/colors
 cp ~/.vim/bundle/vim-hybrid-material/base16-material/base16-material.vim ~/.vim/colors/
+
+# # Python dependency
+# # Installing YouCompleteMe with C support
+# cd ~/.vim/bundle/YouCompleteMe
+# ./install.py --clang-completer
