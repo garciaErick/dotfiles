9cb3b4d5a20b1ec9026355b0290ceb37b0f8a586
Erick Garcia <garcia.erick.cs@gmail.com>
2018-07-28 14:01:49 -0600

    Some refractorings

--- setup.sh
+++ setup.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 cwd=$(pwd)
+zshDir=$cwd/zsh
+vimDir=$cwd/vim
+tmuxDir=$cwd/tmux
+emacsDir=$cwd/emacs
 
 echo "Setting up Tmux configuration"
-ln -sf $cwd/tmux ~/.tmux
-ln -sf $cwd/tmux/tmux.conf ~/.tmux.conf
+ln -sf $tmuxDir ~/.tmux
+ln -sf $tmuxDir/tmux.conf ~/.tmux.conf
 echo "Done \n"
 
 echo "Installing shell color scheme"
-mkdir -p ~/.config/base16-shell
-ln -sf $cwd/colors/base16-material.dark.sh ~/.config/base16-shell/base16-material.dark.sh 
+mkdir -p ~/.config/base16-shell ln -sf $cwd/colors/base16-material.dark.sh ~/.config/base16-shell/base16-material.dark.sh 
 echo "Done \n"
 
 echo "Setting up Zsh configuration"
-ln -sf $cwd/zsh/zshrc ~/.zshrc
+ln -sf $zshDir/zshrc ~/.zshrc
 source ~/.zshrc
 echo "Done \n"
 
 echo "Setting up Vim configuration"
-ln -sf $cwd/vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
-ln -sf $cwd/vim/base16-material.vim ~/.vim/colors
+ln -sf $vimDir/vimrc ~/.vimrc
+ln -sf $vimDir/base16-material.vim ~/.vim/colors
 echo "Done \n"
 
 # Installing vundle
@@ -37,9 +40,9 @@ vim +PluginInstall +qall
 
 echo "Setting up Emacs configuration"
 mkdir -p ~/.emacs.d
-ln -sf $cwd/emacs/configuration.org ~/.emacs.d/.
-ln -sf $cwd/emacs/sensible-defaults.el ~/.emacs.d/.
-ln -sf $cwd/emacs/init.el ~/.emacs.d/.
+ln -sf $emacsDir/configuration.org ~/.emacs.d/.
+ln -sf $emacsDir/sensible-defaults.el ~/.emacs.d/.
+ln -sf $emacsDir/init.el ~/.emacs.d/.
 echo "Done \n"
 
 
