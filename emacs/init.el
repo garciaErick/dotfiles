375093796e8884f4e161e091eede690a122d57f8
Erick Garcia <garcia.erick.cs@gmail.com>
2018-08-22 10:11:26 -0600

    Changed tmux split macro, and added omnisharp

--- emacs/init.el
+++ emacs/init.el
@@ -19,7 +19,7 @@
  '(hl-sexp-background-color "#121212")
  '(package-selected-packages
 			(quote
-				(helm-projectile flycheck helm evil-embrace company expand-region aggressive-indent centered-cursor-mode htmlize org-bullets base16-theme flycheck-color-mode-line powerline material-theme evil auto-compile use-package)))
+				(omnisharp autopair avy helm-projectile flycheck helm evil-embrace company expand-region aggressive-indent centered-cursor-mode htmlize org-bullets base16-theme flycheck-color-mode-line powerline material-theme evil auto-compile use-package)))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
 			(quote
