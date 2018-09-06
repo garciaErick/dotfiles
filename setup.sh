93d598e2ffb10497eeabdc61b4f743cd0542bed7
Erick Garcia <erickcubanoide@gmail.com>
2018-09-06 13:58:48 -0700

    Fixed installing of packages and adding fira code to setup

--- setup.sh
+++ setup.sh
@@ -5,12 +5,20 @@ zshDir=$cwd/zsh
 vimDir=$cwd/vim
 tmuxDir=$cwd/tmux
 emacsDir=$cwd/emacs
+use_proxy=true
 
 # echo "Installing required packages"
-# sudo ./ubuntu_packages/setup.sh
+# cd ubuntu_packages
+# sudo ./setup.sh
+# cd $cwd
 
 # echo "Setting up Zsh as the default shell"
 # chsh -s $(which zsh)
+# echo "Done \n"
+
+# echo "Setting up Zsh oh-my-zsh"
+# git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+# echo "Done \n"
 
 # echo "Setting up Tmux configuration"
 # ln -sf $tmuxDir ~/.tmux
@@ -50,4 +58,16 @@ emacsDir=$cwd/emacs
 echo "Setting up Emacs configuration"
 ln -sf $emacsDir ~/.emacs.d
 mkdir -p ~/.emacs.d/auto-save
+echo "Done \n"
+
+echo "Downloading Firacode fonts for windows"
+wget_args=""
+if [ "$use_proxy" = true ] ; then
+	wget_args="-e use_proxy=yes -e https_proxy=http://web-proxy.houston.hpecorp.net:8080"
+fi
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Bold.ttf $wget_args
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Light.ttf $wget_args
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Medium.ttf $wget_args
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Regular.ttf $wget_args
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Retina.ttf $wget_args
 echo "Done \n"
