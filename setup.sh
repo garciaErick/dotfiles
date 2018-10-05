bfaa9a915488450c05d5d85988d85aa976b1ea82
Erick Garcia <erickcubanoide@gmail.com>
2018-10-05 08:48:06 -0700

    Corrected script

--- setup.sh
+++ setup.sh
@@ -12,72 +12,73 @@ use_proxy=true
 wget_args=""
 if [ "$use_proxy" = true ] ; then
 	wget_args="-e use_proxy=yes -e https_proxy=http://web-proxy.houston.hpecorp.net:8080"
+	sudo cp proxy_settings/* /etc/apt/apt.conf.d/.
 fi
 
-# echo "Installing required packages"
-# cd ubuntu_packages
-# sudo ./setup.sh
-# cd $cwd
+echo "Installing required packages"
+cd ubuntu_packages
+sudo ./setup.sh
+cd $cwd
 
-# echo "Setting up Zsh as the default shell"
-# chsh -s $(which zsh)
-# echo "Done \n"
+echo "Setting up Zsh as the default shell"
+chsh -s $(which zsh)
+echo "Done \n"
 
-# echo "Setting up Zsh oh-my-zsh"
-# git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-# echo "Done \n"
+echo "Setting up Zsh oh-my-zsh"
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+echo "Done \n"
 
-# echo "Setting up Tmux configuration"
-# ln -sf $tmuxDir ~/.tmux
-# ln -sf $tmuxDir/tmux.conf ~/.tmux.conf
-# echo "Done \n"
+echo "Setting up Tmux configuration"
+ln -sf $tmuxDir ~/.tmux
+ln -sf $tmuxDir/tmux.conf ~/.tmux.conf
+echo "Done \n"
 
-# echo "Installing shell color scheme"
-# mkdir -p ~/.config/base16-shell
-# ln -sf $cwd/colors/base16-material.sh ~/.config/base16-shell/base16-material.sh
-# ln -sf $cwd/colors/base16-material-palenight.sh ~/.config/base16-shell/base16-material-palenight.sh
-# ln -sf $cwd/colors/base16-material-palenight-darker.sh ~/.config/base16-shell/base16-material-palenight-darker.sh
-# echo "Done \n"
+echo "Installing shell color scheme"
+mkdir -p ~/.config/base16-shell
+ln -sf $cwd/colors/base16-material.sh ~/.config/base16-shell/base16-material.sh
+ln -sf $cwd/colors/base16-material-palenight.sh ~/.config/base16-shell/base16-material-palenight.sh
+ln -sf $cwd/colors/base16-material-palenight-darker.sh ~/.config/base16-shell/base16-material-palenight-darker.sh
+echo "Done \n"
 
-# echo "Setting up Zsh configuration"
-# ln -sf $zshDir/zshrc ~/.zshrc
-# source ~/.zshrc
-# echo "Done \n"
+echo "Setting up Zsh configuration"
+ln -sf $zshDir/zshrc ~/.zshrc
+source ~/.zshrc
+echo "Done \n"
 
-# echo "Setting up Vim configuration"
-# mkdir -p ~/.vim/colors
-# ln -sf $vimDir/vimrc ~/.vimrc
-# ln -sf $vimDir/base16-material.vim ~/.vim/colors
-# echo "Done \n"
+echo "Setting up Vim configuration"
+mkdir -p ~/.vim/colors
+ln -sf $vimDir/vimrc ~/.vimrc
+ln -sf $vimDir/base16-material.vim ~/.vim/colors
+echo "Done \n"
 
-# # Installing vundle
-# mkdir -p ~/.vim/bundle/
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Installing vundle
+mkdir -p ~/.vim/bundle/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# # Installing Vundle plugins
-# vim +PluginInstall +qall
+# Installing Vundle plugins
+vim +PluginInstall +qall
 
 # # Uncomment to install
 #  Installing YouCompleteMe with C support
 # cd ~/.vim/bundle/YouCompleteMe
 # ./install.py --clang-completer
 
-# echo "Setting up Emacs configuration"
-# ln -sf $emacsDir ~/.emacs.d
-# mkdir -p ~/.emacs.d/auto-save
-# echo "Done \n"
+echo "Setting up Emacs configuration"
+ln -sf $emacsDir ~/.emacs.d
+mkdir -p ~/.emacs.d/auto-save
+echo "Done \n"
 
-# echo "Downloading Firacode fonts for Windows"
-# wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Bold.ttf $wget_args
-# wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Light.ttf $wget_args
-# wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Medium.ttf $wget_args
-# wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Regular.ttf $wget_args
-# wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Retina.ttf $wget_args
-# echo "Done \n"
+echo "Downloading Firacode fonts for Windows"
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Bold.ttf $wget_args .
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Light.ttf $wget_args .
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Medium.ttf $wget_args .
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Regular.ttf $wget_args .
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Retina.ttf $wget_args .
+echo "Done \n"
 
 
 echo "Downloading Autohotkey for Windows"
-wget https://www.autohotkey.com/download/ahk-install.exe $wget_args
+wget https://www.autohotkey.com/download/ahk-install.exe $wget_args .
 cmd.exe /C ahk-install.exe /S
 cp windows/AutoHotkey.ahk /mnt/c/Users/$windowsUser/Documents/
 cp "$autoHotkeyShortcut" "$startupProgramsDir"
--- ubuntu_packages/setup.sh
+++ ubuntu_packages/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#apt update -y
-#apt upgrade -y
+apt update -y
+apt upgrade -y
 apt install -y $(cat packages_to_install.txt) 
