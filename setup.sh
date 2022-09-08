12e1304bdb9e508c981a1604053c8544a60c59be
Erick Garcia <me@erick-garcia.com>
2022-09-07 20:39:42 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ zshDir=$cwd/zsh
 mkdir -p ~/.config/zshrc
 ln -sf $zshDir/.zshrc ~/.zshrc
 
-ln -sf $zshDir/amazon ~/.config/zshrc/amazon
 ln -sf $zshDir/core ~/.config/zshrc/core
 ln -sf $zshDir/ohmyzsh ~/.config/zshrc/ohmyzsh
 ln -sf $zshDir/tmux ~/.config/zshrc/tmux
