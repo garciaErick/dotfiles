85683467b8029b08e17ce19ba9b4f42f1cd90639
Erick Garcia <gazeric@amazon.com>
2022-09-07 18:35:56 -0700

    First real version of setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 cwd=$(pwd)
 zshDir=$cwd/zsh
 
-echo $cwd
-
 mkdir -p ~/.config/zshrc
 ln -sf $zshDir/.zshrc ~/.zshrc
 
@@ -12,3 +10,7 @@ ln -sf $zshDir/amazon ~/.config/zshrc/amazon
 ln -sf $zshDir/core ~/.config/zshrc/core
 ln -sf $zshDir/ohmyzsh ~/.config/zshrc/ohmyzsh
 ln -sf $zshDir/tmux ~/.config/zshrc/tmux
+
+# if Macos
+# ./os-setup/macos/setup
+
